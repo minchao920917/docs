26d62eb496221c7d5ec03d0e6fe5a01aa06c2b25
minchao920917 <499790879@qq.com>
2020-12-07 00:31:26 +0800

    first

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    head: [['link', { rel: 'icon', href: '/logo.ico' }]],
+    // 设置网站标题
+    title: 'mch-ui', 
+    // 设置输出目录
+    dest: './dist', 
+    // 设置站点根路径，如果你在访问的地址是 'www.xxxx.com/wxDocs' 那么就设置成 '/wxDocs/'  
+    base: '/wxDocs/' 
+    // 添加 github 链接，但是这个要放在公司的内网服务器，所以为空
+    // repo: '' 
+  }
+  
