075477b742c57eee8e596f141ec80155db8a1f36
minchao920917 <499790879@qq.com>
2020-12-08 00:05:42 +0800

    docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,8 +5,35 @@ module.exports = {
     // 设置输出目录
     dest: './dist', 
     // 设置站点根路径，如果你在访问的地址是 'www.xxxx.com/wxDocs' 那么就设置成 '/wxDocs/'  
-    base: '/wxDocs/' 
-    // 添加 github 链接，但是这个要放在公司的内网服务器，所以为空
     // repo: '' 
+
+    themeConfig: {
+        // 添加导航栏
+        nav: [
+
+          { text: '首页', link: '/' },
+          { text: '组件', link: '/components/' },
+          { text: '博客', link: 'http://minchao.me' },
+          {
+            text: 'github',
+            // 这里是下拉列表展现形式。
+            items: [
+              { text: 'mch-ui', link: 'https://github.com/minchao920917/mch-ui' },
+              { text: 'docs', link: 'https://github.com/minchao920917/docs' }
+            ]
+          }
+        ],
+        // 为以下路由添加侧边栏
+        sidebar: {
+            '/one/': [
+             
+            ],
+            '/two/': [
+              
+            ]
+          },
+          sidebarDepth: '0',
+      },
+      
   }
   
